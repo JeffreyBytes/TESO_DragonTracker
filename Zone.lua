698e6a1934a8932ede3b0e9ef528c7d6b2d5c2b2
bulton-fr <bulton.fr@gmail.com>
2019-09-08 15:51:43 +0200

    Zone : Move check zone with dragon to dedicated method, and improve check
    Use GetNextWorldEventInstanceId() to know if their are dragons because currently only dragons are WorldEvent.

--- Zone.lua
+++ Zone.lua
@@ -5,13 +5,8 @@
 --]]
 function DragonTracker:checkZone()
     local currentMapZoneIdx = GetCurrentMapZoneIndex()
-    local currentMapCtnType = GetMapContentType()
-    self.zoneInfo.onDragonZone = false
-
-    -- Check if it's a zone with Dragon
-    if currentMapZoneIdx == 680 and currentMapCtnType == 0 then -- Elsweyr
-        self.zoneInfo.onDragonZone = true
-    end
+    
+    self:CheckZoneWithDragons(currentMapZoneIdx)
 
     -- Check parent zone changed (function also called with sub-zone change) to reset dragon info
     if self.zoneInfo.lastMapZoneIdx ~= currentMapZoneIdx then
@@ -21,4 +16,23 @@ function DragonTracker:checkZone()
 
     DragonTracker:changeTimerStatus(DragonTracker.zoneInfo.onDragonZone)
     DragonTracker:GuiShowHide(DragonTracker.zoneInfo.onDragonZone)
+end
+
+--[[
+-- Check if it's a zone with Dragon.
+-- Currently only Elsweyr.
+--
+-- @param integer currentMapZoneIdx zone index
+--]]
+function DragonTracker:CheckZoneWithDragons(currentMapZoneIdx)
+    self.zoneInfo.onDragonZone = false
+    
+    -- No world event (currently aka Dragon) on the zone
+    if GetNextWorldEventInstanceId() == nil then
+        return
+    end
+
+    if currentMapZoneIdx == 680 then -- Elsweyr
+        self.zoneInfo.onDragonZone = true
+    end
 end
