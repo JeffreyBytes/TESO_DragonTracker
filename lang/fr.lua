3558524d6fa3712fef4220e4599514ef0426b400
bulton-fr <bulton.fr@gmail.com>
2019-09-17 18:23:37 +0200

    lang/fr : Typo

--- lang/fr.lua
+++ lang/fr.lua
@@ -10,7 +10,7 @@ ZO_CreateStringId("SI_DRAGON_TRACKER_STATUS_FIGHT",   "En combat")
 ZO_CreateStringId("SI_DRAGON_TRACKER_STATUS_KILLED",  "Tué")
 ZO_CreateStringId("SI_DRAGON_TRACKER_STATUS_UNKNOWN", "Inconnu")
 ZO_CreateStringId("SI_DRAGON_TRACKER_STATUS_WAITING", "En attente ou en vol")
-ZO_CreateStringId("SI_DRAGON_TRACKER_STATUS_WEAK",    "En combat (vie < 50%)")
+ZO_CreateStringId("SI_DRAGON_TRACKER_STATUS_WEAK",    "En combat (santé < 50%)")
 
 -- Timer
 ZO_CreateStringId("SI_DRAGON_TRACKER_TIMER_SINCE",  "depuis")
